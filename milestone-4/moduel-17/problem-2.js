22af9b13f9ee1dcabd0b426bad45e756f0d4cb12
Indrajit Chandra Chanda <indrajitchandra98@gmail.com>
2022-08-03 22:38:41 +0600

    milestone-4

---
+++ milestone-4/moduel-17/problem-2.js
@@ -0,0 +1,5 @@
+var Self = 85;
+var Tom = 66;
+var Jane = 95;
+var Peter = 56;
+var John = 40;
---
+++ milestone-4/moduel-18/index.js
@@ -0,0 +1,56 @@
+// var number = 0;
+// while(number<5){
+//     console.log(number + " Indrajit")
+//     number ++
+// }
+
+// var number = 0;
+// while(number <= 50){
+//     console.log(number);
+//     number=number + 2;
+// }
+
+// var arr = [ 45, 54, 66, 87, 59, 88, 87,54, 82, 75 ];
+
+// for(var i=0; i<arr.length; i++){
+//     var result = arr[i];   
+//     console.log(result)
+// }
+
+// var i = 0;
+// for (i=0; i<5; i++){
+
+// };
+// console.log(i);
+
+// for(var i= 0; i<=10; i++){
+//     console.log(i)
+// }
+
+// var name = "false";
+
+// console.log( typeof "false")
+
+// var i = 5;
+// for ( i=3; i < 5; i++) {
+//   console.log(i);
+// }
+
+// var i = 5;
+// for (i < 5; i++) {
+//   console.log(i);
+// }
+var Name = [ 'indrajit','joudip','mohon','dipto'];
+var name2 = [];
+for(var i=0; i<Name.length; i++){
+  // console.log(Name[i])
+   name2.push(Name[i])
+}
+console.log(name2)
+// console.log(Name)
+// Name.push("chanda")
+// console.log(Name)
+// aa= Name.indexOf("mohon")
+// console.log(aa)
+// bb= Name[5]
+// console.log(bb)
