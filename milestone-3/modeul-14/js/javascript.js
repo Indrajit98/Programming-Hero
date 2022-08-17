df29c7835bd79de53bc424f6d608b4862f44de3a
Indrajit Chandra Chanda <indrajitchandra98@gmail.com>
2022-08-18 03:19:20 +0600

    modeul -14 javascript add update

---
+++ milestone-3/modeul-14/js/javascript.js
@@ -0,0 +1,5 @@
+document.getElementById('btn-buy-now').addEventListener('click', function(){
+    const headerTitle = document.getElementById('header-title');
+    headerTitle.innerText = "I am penguins" ;
+    
+})
