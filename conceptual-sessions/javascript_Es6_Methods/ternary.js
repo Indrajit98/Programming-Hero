b3c94303ce2e0e15d116e93d4e8d3ddce7133bb4
Indrajit Chandra Chanda <indrajitchandra98@gmail.com>
2022-08-26 23:44:48 +0600

    conceptual sessions add

---
+++ conceptual-sessions/javascript_Es6_Methods/ternary.js
@@ -0,0 +1,11 @@
+//ternary operator 
+const isStudent = true;
+if(isStudent ===true){
+    console.log('he is a student');
+}
+else{
+    console.log('he is not a student');
+}
+
+const sentence = isStudent === true ? 'he is a student' : 'he is not a student';
+console.log(sentence)
---
+++ conceptual-sessions/javascript_Es6_Methods/test.js
@@ -0,0 +1,5 @@
+const number = [54,64,+2,54,21,34,21,51,34,68,32]
+const result = number.forEach(items => items *2)
+const result2 = number.map(items => items *2)
+console.log(result)
+console.log(result2)
