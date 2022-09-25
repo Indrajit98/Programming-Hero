2dc1ea5983440a0c0698261cb3b0e91d28592e87
Indrajit Chandra Chanda <indrajitchandra98@gmail.com>
2022-09-26 04:11:51 +0600

    module-49

--- milestone-8/module-48/ema-jhon-resources/src/components/Shop/Shop.js
+++ milestone-8/module-48/ema-jhon-resources/src/components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -9,6 +10,7 @@ const Shop = () => {
 
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
+    // console.log(products)
 
     useEffect ( () => {
         fetch('products.json')
@@ -16,10 +18,32 @@ const Shop = () => {
         .then(data => setProducts(data))
     }, [])
 
+    useEffect ( () =>{
+        const storedCart = getStoredCart();
+        const saveCart = [];
+
+        // console.log(storedCart);
+        for (const id in storedCart){
+            const addedProduct = products.find(product => product.id ===id)
+            if(addedProduct){
+                const quantity = storedCart[id]
+                addedProduct.quantity = quantity;
+                saveCart.push(addedProduct);
+                console.log(addedProduct);
+            }
+        }
+        setCart(saveCart)
+
+
+    } ,[products])
+
     const handleAddToCart =(product) => {
         // console.log(product)
         const newCart = [...cart,product];
         setCart(newCart);
+
+        addToDb(product.id)
+
     }
 
 
